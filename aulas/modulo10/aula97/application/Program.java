ed5ba227e3954694d91720ded8c4d48644a5592b
tenchini <viniciustenchi@gmail.com>
2026-07-30 14:57:32 -0300

    feat: add `.md` file and practice for lesson 97

---
+++ aulas/modulo10/aula97/application/Program.java
@@ -0,0 +1,15 @@
+package modulo10.aula97;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    static void main() {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+
+
+        sc.close();
+    }
+}
---
+++ aulas/modulo10/aula97/entities/Product.java
@@ -0,0 +1,4 @@
+package modulo10.aula97.entities;
+
+public class Product {
+}
