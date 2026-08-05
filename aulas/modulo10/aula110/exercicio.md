77a55adbfe3410fc9a8fcf9faca7cfde63495794
tenchini <viniciustenchi@gmail.com>
2026-08-05 15:39:52 -0300

    feat: add solution for class 110 exercise

---
+++ aulas/modulo10/aula110/exercicio.md
@@ -0,0 +1,8 @@
+# Exercício resolvido
+
+---
+Fazer um programa para ler um número inteiro N e uma matriz de ordem N contendo números inteiros.
+Em seguida, mostrar a diagional principal e a quantidade de valores negativos da matriz
+
+
+![img.png](img.png)
--- aulas/modulo10/aula110/exercicioresolvido/Program.java
+++ aulas/modulo10/aula110/exercicioresolvido/Program.java
@@ -1,4 +1,40 @@
 package modulo10.aula110.exercicioresolvido;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[][] matriz = new int[n][n];
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Main diagonal: ");
+        for (int i = 0; i < matriz.length; i++) {
+            System.out.print(matriz[i][i] + " ");
+        }
+        System.out.println();
+
+        int count = 0;
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                if (matriz[i][j] < 0) {
+                    count++;
+                }
+            }
+        }
+        System.out.println("Negative numbers = " + count);
+
+
+
+        sc.close();
+    }
 }
